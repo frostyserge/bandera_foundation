06d26b12ab42c094d146b0463c4eda151d6671b7
frostyserge <eyemoroz@gmail.com>
2023-07-16 23:16:48 -0400

    populated database with basic data for test. updated views, urls

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),
-    path('shop/', views.MerchList.as_view(), name = 'shop'),
+    path('shop/', views.MerchList.as_view(), name = 'merch_list'),
     path('shop/<int:pk>/', views.MerchDetail.as_view(), name = 'merch_detail'),
     path('logout/', views.LogoutView.as_view(), name = 'logout'),
 ]
